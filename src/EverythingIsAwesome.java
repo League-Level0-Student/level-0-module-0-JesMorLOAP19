98c0e12706a03a70f4d165ae8a3f15f1f801fad9
ilabstaff <ilabstaff@172.16.116.68>
2019-08-17 10:52:28 -0700

    cs

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+	public static void main(String[] args) {
+String input=JOptionPane.showInputDialog("what do you like"); 
+JOptionPane.showMessageDialog(null,input +" is awesome!");
+	
+	
+	}
+}
---
+++ src/summer.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class summer {
+public static void main(String[] args) {
+	String input=JOptionPane.showInputDialog("what did you do this summer");
+JOptionPane.showMessageDialog(null,input+"i know you went swimming,"+input+"!");
+}}
